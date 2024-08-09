46dcb85318470b46a186458fc072e9f3f36c74c5
Richard Blackwell <rdblackwell@sky.com>
2024-08-09 16:11:47 +0100

    New example

--- example001/app.R
+++ example001/app.R
@@ -1,36 +1,58 @@
 library(shiny)
 library(plotly)
 
+# Load the organisation map
+df_org_map <- read.csv('www/MAPPING_NHS_GEOGRAPHIES_2223.csv')
+region_list <- df_org_map %>% arrange(REGION_NAME) %>% distinct(REGION_ODS_CODE) %>% .$REGION_ODS_CODE
+names(region_list) <- df_org_map %>% arrange(REGION_NAME) %>% distinct(REGION_NAME) %>% .$REGION_NAME
+
+# Load the prevalence data
+df_prev <- read.csv('www/PREVALENCE_2223.csv')
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     # Sidebar layout 
     sidebarLayout(
         sidebarPanel(
-          numericInput(inputId = 'numMean', label = 'Mean', 
-                       min = 0, max = 100,
-                       value = 0, step = 1),
-          numericInput(inputId = 'numSD', label = 'SD', 
-                       min = 0, max = 50,
-                       value = 1, step = 0.1),
-          numericInput(inputId = 'numSize', label = 'Size', 
-                       min = 0, max = 10000,
-                       value = 10, step = 10),
+          selectInput(inputId = 'edtRegion', 
+                      label = 'Region', 
+                      choices = region_list,
+                      selected = 1
+                      ),
+          selectInput(inputId = 'edtICB', 
+                      label = 'ICB', 
+                      choices = '',
+                      selected = 1
+          ),
           width = 2
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotlyOutput(outputId = 'pltSample', width = '100%', height = '900px')
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  output$pltSample <- renderPlotly({
-    x <- rnorm(n = input$numSize, mean = input$numMean, sd = input$numSD)
-    plot_ly(x = ~x, type = "histogram")
-  })
+
+  observeEvent(input$edtRegion,
+          {
+            icb_list <- df_org_map %>% 
+              dplyr::filter(REGION_ODS_CODE==input$edtRegion) %>% 
+              arrange(ICB_NAME) %>% 
+              distinct(ICB_ODS_CODE) %>% 
+              .$ICB_ODS_CODE
+            
+            names(icb_list) <- df_org_map %>% 
+              dplyr::filter(REGION_ODS_CODE==input$edtRegion) %>% 
+              arrange(ICB_NAME) %>% 
+              distinct(ICB_NAME) %>% 
+              .$ICB_NAME
+            
+            updateSelectInput(inputId = 'edtICB', choices = icb_list, selected = 1)
+            
+          })
 }
 
 # Run the application 
